686b3f726cab564365c567d80b6e7d60c8dd4bd1
João Pedro Silva Cunha <jpcunha5@gmail.com>
2022-10-15 17:42:10 -0300

    Iniciando calsse Produto

--- src/models/Produto.php
+++ src/models/Produto.php
@@ -4,4 +4,60 @@ namespace src\models;
 
 class ProdutoModel
 {
+  private string $nomeProduto;
+  private float  $precoProduto;
+  private  int $quantidadeProduto;
+  private string $descricaoProduto;
+  private int $idProduto;
+
+  public function setNomeProduto(string $nomeProduto)
+  {
+    $this->nomeProduto = $nomeProduto;
+  }
+
+
+  public function setPrecoProduto(float $precoProduto)
+  {
+    $this->precoProduto = $precoProduto;
+  }
+
+
+  public function setQuantidadeProduto(int $quantidadeProduto)
+  {
+    $this->quantidadeProduto = $quantidadeProduto;
+  }
+
+
+  public function setDescricaoProduto(string $descricaoProduto)
+  {
+    $this->descricaoProduto = $descricaoProduto;
+  }
+
+  public function getNomeProduto()
+  {
+    return $this->nomeProduto;
+  }
+
+
+  public function getPrecoProduto()
+  {
+    return $this->precoProduto;
+  }
+
+
+  public function getQuantidadeProduto()
+  {
+    return $this->quantidadeProduto;
+  }
+
+
+  public function getDescricaoProduto()
+  {
+    return $this->descricaoProduto;
+  }
+
+  public function getIdProduto()
+  {
+    return $this->idProduto;
+  }
 }

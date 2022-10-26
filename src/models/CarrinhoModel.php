f98dbc0312e3054ea65ff1715c5f501b3833ec44
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-10-26 20:18:58 -0300

    Update CarrinhoModel.php

--- src/models/CarrinhoModel.php
+++ src/models/CarrinhoModel.php
@@ -1,7 +1,30 @@
 <?php
 
+// É dado um nome para o documento CarrinhoModel.php . Esse nome é o que nós iremos nos referir no use src\ .
+
 namespace src\models;
 
-class CarrinhoModel
-{
+// Usaremos a classe de conexão com o banco de dados que está no heroku e também o pdo, e a classe do CarrinhoData.
+
+use src\database\CarrinhoData;
+use src\config\Connection;
+
+// Por um motivo que desconheço foi necessário incluir o CarrinhoData.php por este método também.
+
+include ("database/CarrinhoData.php");
+
+class CarrinhoModel {
+	
+	// Nesta função nós criamos o objeto execution, executamos a função addCarrinho definida no CarrinhoData.php, passando o id do
+	// produto dado pelo CarrinhoController.
+	
+	public function execute ($id_produto) {
+		
+		$execution = new CarrinhoData ();
+		$execution -> addCarrinho($id_produto);
+		
+	}
+	
 }
+
+?>

c9d5e863549dd95edcb9a32d4b8c292c4335d295
Edson Júnior <edsonjunior.1219@gmail.com>
2022-10-27 00:29:03 -0300

    feat(src/models/CarrinhoModel.php): create functions of cart model

--- src/models/CarrinhoModel.php
+++ src/models/CarrinhoModel.php
@@ -1,7 +1,30 @@
 <?php
 
+
 namespace src\models;
+
+use src\database\CarrinhoData;
+
+include 'database/CarrinhoData.php';
 
 class CarrinhoModel
 {
+  public function delete($userId, $productId)
+  {
+    $cartData = new CarrinhoData();
+    $cartData->removeFromCart($userId, $productId);
+  }
+
+  public function update($quantity, $userId, $productId)
+  {
+    $cartData = new CarrinhoData();
+    $cartData->updateCartProduct($quantity, $userId, $productId);
+  }
+
+  public function findOne($userId, $productId)
+  {
+    $cartData = new CarrinhoData();
+    $response = $cartData->getProduct($userId, $productId);
+    return $response;
+  }
 }

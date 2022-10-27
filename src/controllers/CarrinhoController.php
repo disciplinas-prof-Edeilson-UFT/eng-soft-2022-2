a678ab2f75aeba83e536da3acd90903cfbd0d152
ianasampaio <iana.sampaio06@gmail.com>
2022-10-27 12:52:01 -0300

    feat(src/controllers/CarrinhoController.php) add functions to cart controller

--- src/controllers/CarrinhoController.php
+++ src/controllers/CarrinhoController.php
@@ -2,6 +2,45 @@
 
 namespace src\controllers;
 
+use src\models\CarrinhoModel;
+
+include 'models/CarrinhoModel.php';
+
 class CarrinhoController
 {
+    private static $cartModel;
+
+    public function __construct(){
+        $this->cartModel = new CarrinhoModel();
+    }
+
+    private function productExists($userId, $productId) {
+        $response = $this->cartModel.findOnde($userId, $productId);
+        if (!$response) {
+            return "Product is not in cart";
+        }
+        return $response;
+    }
+
+    public function removeProduct($userId, $productId) {
+        $productExists = $this->productExists($userId, $productId);
+        if (!$productExists) {
+            return $productExists;
+        }
+        $this->cartModel.delete($userId, $productId);
+        return "Product deleted with success";
+    }
+
+    public function removeSomeProducts($quantity, $userId, $productId) {
+        $productExists = $this->productExists($userId, $productId);
+        if (!$productExists) {
+            return $productExists;
+        }
+
+        if (!$productExists["quantidade_item_carrinho"] >= $quantity) {
+            return "Could not remove product quantity";
+        }
+
+        $this->cartModel.update($quantity, $userId, $productId);
+    }
 }

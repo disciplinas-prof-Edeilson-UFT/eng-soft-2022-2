df8978f834af997794879d04e9555e4a83d8909c
Izann <izannrogerio@gmail.com>
2022-10-31 19:43:09 -0300

    Estilizando página da listagem de produtos

--- src/views/pages/ListaProdutos.php
+++ src/views/pages/ListaProdutos.php
@@ -1,96 +1,47 @@
 <?php
-use src\models\ProdutoModel;
-$model = new ProdutoModel();
+
 include './views/templates/cabecalho.php';
 
-if(!empty($_GET['search'])){
-  $data = $_GET['search'];
-   $model = $model->getById($data);
- }else{
-  $model->rows = $model->rows;
-}
 ?>
 
 <head>
   <link rel="stylesheet" href="./views/css/ListaProdutos.css">
 </head>
 
- 
+<body class="global">
+  <div class="container">
+    <div class="box-search">
+      <input type="search" placeholder="Busque aqui" id="pesquisar">
+      <button id="but" onclick="searchData()">Buscar</button>
+    </div>
+    <div class="prodGrid">
+      <table>
+        <?php foreach ((array)$model->rows as $item) : ?>
+          <div class="gridItem">
+            <img src="https://uploaddeimagens.com.br/images/004/084/732/full/produtoimagem.png?1667238087">
+            <div class="content">
+              <div class="info">
+                <p><?= $item->nome_produto ?> </p>
+                <p><?= $item->descricao_produto ?></p>
+                <p> R$<?= $item->preco_produto ?></p>
+              </div>
+              <div class="button">
+                <a href="/produto/unique?id_produto=<?= $item->id_produto ?>">Comprar</a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
 
-<body>
-
-   <div class="box-search">
-    <input type="search" placeholder="Busque aqui" id="pesquisar">
-    <button onclick="searchData()">Buscar</button>
-  </div>
-
-  <div id="container">
-    <header>
-    <main>
-      <header>Produtos x</header>
-      
-      <section id="produtos">
-          <?php foreach ((array)$model->rows as $item) : ?>
-      <tr>
-        <section class="produtos">
-        <section class="produto">
-        <img src="./views/assets/img.jpg" width="195px">
-        <p> <?= $item->nome_produto ?> </p>
-        <p><?= $item->descricao_produto ?></p>
-        <p> R$<?= $item->preco_produto ?></p>        
-        <!-- <p><?= $item->id_produto ?> </p> -->
-        <a href="/produto/unique?id_produto=<?= $item->id_produto ?>"><button>Comprar</button></a>
-        </section>
-        </section>
-      </tr>
-    <?php endforeach; ?>
-      
-
-      </section>
-    </main>
-  </div>
-
-  <div id="container">
-    <header>
-    <main>
-      <header>Produtos y</header>
-      
-      <section id="produtos">
-          <?php foreach ((array)$model->rows as $item) : ?>
-      <tr>
-        <section class="produtos">
-        <section class="produto">
-        <img src="./views/assets/nt.jpg" width="195px">
-        <p> <?= $item->nome_produto ?> </p>
-        <p><?= $item->descricao_produto ?></p>
-        <p> R$<?= $item->preco_produto ?></p>        
-        <!-- <p><?= $item->id_produto ?> </p> -->
-        <a href="/produto/unique?id_produto=<?= $item->id_produto ?>"><button>Comprar</button></a>
-        </section>
-        </section>
-      </tr>
-    <?php endforeach; ?>
-      
-
-      </section>
-    </main>
+      </table>
+    </div>
   </div>
 </body>
-
-
 <script>
   var search = document.getElementById('pesquisar');
-  function searchData(){
-    window.location = 'produto?search='+search.value;
+
+  function searchData() {
+    window.location = 'produto?search=' + search.value;
   }
 </script>
 
 </html>
-
-
-<!-- 
- -->
-
-
-<!-- 
-   -->

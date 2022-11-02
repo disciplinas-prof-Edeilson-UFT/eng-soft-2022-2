2d9b0d95455bda926410499502e8174c05fd9d35
luiz fernando pires kozak <102630535+fernandokozak@users.noreply.github.com>
2022-11-02 13:02:09 -0300

    Enviando estilo para página de visualização do carrinho

--- src/views/pages/VisualizarCarrinho.php
+++ src/views/pages/VisualizarCarrinho.php
@@ -3,7 +3,7 @@
 use src\controllers\CarrinhoController;
 
 include './views/templates/cabecalho.php';
-include ("controllers/CarrinhoController.php");
+include("controllers/CarrinhoController.php");
 
 if ($_POST) {
 
@@ -18,73 +18,78 @@ if ($_POST) {
 
 <html>
 
-	<head>
+<head>
 
-		<script>
+    <link rel="stylesheet" href="../views/css/VisualizarCarrinho.css">
+    <script>
+        if (window.history.replaceState) {
 
-			if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
 
-				window.history.replaceState(null, null, window.location.href);
-			}
+</head>
 
-		</script>
-	
-	</head>
+<body class="global">
 
-<body>
-    <div>
-        <h3>SELECIONE O ENDEREÇO</h3>
-        <input type="text" required>
-        <button type="submit">Ok</button>
-    </div>
-    <div>
-        <h3>PRODUTO E FRETE</h3>
-        <form method="POST">
-            <button name="removecart" type="submit">REMOVER TODOS OS PRODUTOS</button>
-        </form>
-        <table>
-            <?php
-            $carrinhoController = new CarrinhoController();
-            $rows = $carrinhoController->selecionaCarrinho();
-            foreach ((array)$rows as $item) : ?>
+    <div class="container">
+        <div class="addres">
+            <h3>SELECIONE O ENDEREÇO</h3>
+            <input type="text" required>
+            <button type="submit">Ok</button>
+        </div>
+        <div class="prod">
+            <h3>PRODUTO E FRETE</h3>
+            <table>
                 <tr>
                     <th>Nome</th>
                     <th>Quantidade</th>
                     <th>Preço</th>
                 </tr>
+                <?php
+                $carrinhoController = new CarrinhoController();
+                $rows = $carrinhoController->selecionaCarrinho();
+                foreach ((array)$rows as $item) : ?>
+                    <tr>
+                        <td> <?= $item->nome_produto ?></td>
+                        <td> <?= $item->quantidade_item_carrinho ?></td>
+                        <td> R$<?= $item->preco_produto ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+            <form class="bot" method="POST">
+                <button name="removecart" type="submit">REMOVER TODOS OS PRODUTOS</button>
+            </form>
+            <h4>FRETE:</h4>
+        </div>
+        <div class="nutshell">
+            <h3>RESUMO</h3>
+            <p>Valor dos Produtos:
 
-                <tr>
-                    <td> <?= $item->nome_produto ?></td>
-                    <td> <?= $item->quantidade_item_carrinho ?></td>
-                    <td> <?= $item->preco_produto ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <h4>FRETE:</h4>
-    </div>
-    <div>
-        <h3>RESUMO</h3>
-		<p>Valor dos Produtos: 
-		
-			<?php
-			
-				// Criamos o objeto value e chamamos a classe do carrinho controllee, que vai chamar a função de mostrar o preco.
-				// O valor retornado está numa array, onde na posição 0 está o nosso objeto, e dentro dele temos a propriedade sum
-				// que foi obtida na query com nosso banco de dados. Nessa propriedade está o valor total do carrinho.
-			
-				$value = new CarrinhoController ();
-				$value = $value -> showPrice ();
-        
-				echo ($value[0]-> sum);
-			
-			?> 
-		</p>
-        <p>Frete:</p>
-        <h4>Total:</h4>
-        <button type="button">IR PARA O PAGAMENTO</button>
-        <a href="/produto"><button type="button">CONTINUAR COMPRANDO</button></a>
-    </div>
+                <?php
 
+                // Criamos o objeto value e chamamos a classe do carrinho controllee, que vai chamar a função de mostrar o preco.
+                // O valor retornado está numa array, onde na posição 0 está o nosso objeto, e dentro dele temos a propriedade sum
+                // que foi obtida na query com nosso banco de dados. Nessa propriedade está o valor total do carrinho.
+
+                $value = new CarrinhoController();
+                $value = $value->showPrice();
+
+                function formatar($val)
+                {
+                    return number_format($val, 2, '.', '');
+                }
+
+                echo "R$" . formatar($value[0]->sum);
+
+                ?>
+            </p>
+            <p>Frete:</p>
+            <h4>Total:</h4>
+            <button type="button">IR PARA O PAGAMENTO</button>
+            <a href="/produto"><button type="button">CONTINUAR COMPRANDO</button></a>
+        </div>
+    </div>
 </body>
 
 </html>

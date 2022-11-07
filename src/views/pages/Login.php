48fd6f94164bc4e317e002c15bade6f7a035793a
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-07 20:27:16 -0300

    View do login

---
+++ src/views/pages/Login.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once ("vendor/autoload.php");
+
+?>
+
+<html>
+
+	<head>
+	
+		<link rel = "stylesheet" href = "./views/css/Login.css">
+	
+	</head>
+	
+	<body>
+	
+		<div>
+		
+			<img src = "https://uploaddeimagens.com.br/images/004/064/618/thumb/user.png?1666047148">
+	
+			<form method = "POST">
+		
+				<input type = "email" placeholder = "email">
+				<input type = "password" placeholder = "senha">
+				<input class = "button" type = "submit" value = "Login">
+		
+			</form>
+		
+		</div>
+	
+	
+	</body>
+
+</html>

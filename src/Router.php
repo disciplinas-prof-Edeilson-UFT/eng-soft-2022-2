42da18e51b97d1a21defd13cd933517ab2c07916
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-07 23:51:21 -0300

    Rota do login adicionada

--- src/Router.php
+++ src/Router.php
@@ -4,8 +4,9 @@ namespace src;
 
 use src\routes\ProdutoRoute;
 use src\routes\CarrinhoRoute;
+use src\routes\UsuarioRoute;
 
-include __DIR__ . '/./views/templates/cabecalho.php';
+session_start ();
 
 class Router
 {
@@ -33,14 +34,18 @@ class Router
 
     switch ($var[1]) {
       case self::PRODUTO:
+        include __DIR__ . '/./views/templates/cabecalho.php';
         $instancia = new ProdutoRoute($this->method, $this->payload, $this->query, $var[2]);
         $instancia->produtoRouting();
         break;
       case self::CARRINHO:
+        include __DIR__ . '/./views/templates/cabecalho.php';
         $instancia = new CarrinhoRoute($this->method, $this->payload, $this->query);
         $instancia->carrinhoRouting();
         break;
       case self::USUARIO:
+        $instancia = new UsuarioRoute ($this->method, $this->payload, $this->query);
+        $instancia->usuarioRouting();
         break;
     }
   }

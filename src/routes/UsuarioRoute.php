60c3302e9080c9c061bbfc44d39028679921880d
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-07 23:52:03 -0300

    Rota do login criada

---
+++ src/routes/UsuarioRoute.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace src\routes;
+
+require_once 'vendor/autoload.php';
+
+class UsuarioRoute
+{
+  private $method;
+  private $payload;
+  private $params;
+  const GET = 'get';
+  const POST = 'post';
+  const PATCH = 'patch';
+  const DELETE = 'delete';
+
+  public function __construct($method, $payload, $params)
+  {
+    $this->method = $method;
+    $this->payload = $payload;
+    $this->params = $params;
+  }
+
+  public function usuarioRouting()
+  {
+    switch ($this->method) {
+      case self::GET:
+        include __DIR__ . '/../views/pages/Login.php';
+        break;
+
+      case self::POST:
+        include __DIR__ . '/../views/pages/Login.php';
+
+        break;
+
+      case self::PATCH:
+
+        break;
+
+      case self::DELETE:
+
+        break;
+
+      default:
+        break;
+    }
+  }
+}

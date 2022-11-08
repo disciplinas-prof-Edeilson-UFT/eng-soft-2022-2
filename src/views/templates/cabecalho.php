56aa7dce65c57166dc44272bf84757a84019aa11
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-08 20:13:11 -0300

    Cabecalho com logout

--- src/views/templates/cabecalho.php
+++ src/views/templates/cabecalho.php
@@ -1,9 +1,32 @@
+<?php 
+		
+	if (isset ($_GET ["logout"]) && ($_SESSION ["id"] != NULL)) {
+		
+		session_destroy ();
+		
+	}
+?>
+
 <html lang="pt-br">
 
 <head>
+
   <meta charset="UTF-8">
   
   <title>EngSoft</title>
+  
+  <script>
+        if (window.history.replaceState) {
+        	
+            <?php 
+            
+            	session_commit ();
+            
+            ?>;
+        }
+        
+        
+    </script>
   
   <style>
   
@@ -24,12 +47,12 @@
   padding: 0;
   font-family: 'Inter', sans-serif;
   }
-    
-    body {
-    
-      background : #021b2b;
-      
-    }
+  
+  body {
+  	
+  	background : #021b2b;
+  	
+  }
   
   .cabeçalho {
   background: linear-gradient(#02031a, #021b2b);
@@ -57,7 +80,46 @@
   background : #b10c43;
   border-radius : 30px;
   }
-
+  
+  .profile div {
+  	
+  	height: 0%;
+  	width : 0%;
+  	background : #b10c43;
+  	margin-top : -18px;
+  	border : 2px #ff0841 solid;
+  	border-radius : 15px;
+  	transition : 0.2s;
+  	
+  }
+  
+  
+  .profile:hover div{
+  	
+  	position : fixed;
+  	width : 15%;
+  	height : 40%;
+  	transition : 0.2s;
+  	
+  }
+  
+  .profile input {
+  	
+  	display : none;
+  	background : none;
+  	border : none;
+  	color : #021b2b;
+  	font-size : 40px;
+  	
+  	
+  }
+  
+  .profile:hover input{
+  	
+  	display : flex;
+  	border-bottom : 2px #021b2b solid;
+  	
+  }
 
   .sideR {
   display: flex;
@@ -71,15 +133,31 @@
   }
   
   </style>
+  
 </head>
 
 <header>
   <div class = "cabeçalho">
     <a class = "logo" href="/produto">LOGO</a>
     <div class="sideR">
-      <a href="#" class = "profile">
+    
+      <nav class = "profile">
+      
         <img src="https://uploaddeimagens.com.br/images/004/064/618/thumb/user.png?1666047148">
-      </a>
+        
+        	<div>
+        	
+        		<form method = "GET" >
+        		
+        			<input class = "button" type = "submit" name = "logout" value = "logout">
+        		
+        		</form>
+        			
+        	
+        	</div>
+        
+      </nav>
+      
       <a href="/carrinho" class=" buy">
         <img src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG60.png">
       </a>

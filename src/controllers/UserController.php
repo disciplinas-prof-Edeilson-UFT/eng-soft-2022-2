bacd052fb2d4ca8e15b68ddfa48e330a50a53a1e
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-07 23:54:03 -0300

    Controller do usuário criado

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,8 +2,22 @@
 
 namespace src\controllers;
 
+use src\models\UserModel;
+
 require_once 'vendor/autoload.php';
 
-class UserController
-{
+class UserController {
+	
+	public function login ($name, $password) {
+		
+		// É retornado para a view o resultado da model.
+		
+		$user = new UserModel ();
+		
+		$user = $user -> login ($name, $password);
+		
+		return $user;
+	}
+	
+	
 }

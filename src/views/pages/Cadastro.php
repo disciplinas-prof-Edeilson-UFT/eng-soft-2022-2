2ebb8fb8452789754718603ab9c24e37ed9b58f7
LarissaHirai <91437673+LarissaHirai@users.noreply.github.com>
2022-11-10 13:20:57 -0300

    Update página de cadastro

--- src/views/pages/Cadastro.php
+++ src/views/pages/Cadastro.php
@@ -25,67 +25,9 @@ require_once 'vendor/autoload.php';
         <form method = "POST">
             <input type = "nome" placeholder = "nome">
             <input type = "cpf" placeholder = "cpf">
-            <input class = "button" type = "submit" value = "cadastro">
-    
+	    <input type="hidden" name="opcao" value="cadastro">
+            <input type="submit" name="enviar" value="Enviar" class="btn btn-outline-secondary btn-lg btn-block mb-5">
         </form>
-    
     </div>
-    <!--<a href="home.php">Voltar</a>
-    <div class="box">
-        <form action="formulario.php" method="POST">
-            <fieldset>
-                <legend><b>Fórmulário de Clientes</b></legend>
-                <br>
-                <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Nome completo</label>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <input type="password" name="senha" id="senha" class="inputUser" required>
-                    <label for="senha" class="labelInput">Senha</label>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="text" name="email" id="email" class="inputUser" required>
-                    <label for="email" class="labelInput">Email</label>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
-                    <label for="telefone" class="labelInput">Telefone</label>
-                </div>
-                <p>Sexo:</p>
-                <input type="radio" id="feminino" name="genero" value="feminino" required>
-                <label for="feminino">Feminino</label>
-                <br>
-                <input type="radio" id="masculino" name="genero" value="masculino" required>
-                <label for="masculino">Masculino</label>
-                <br>
-                <input type="radio" id="outro" name="genero" value="outro" required>
-                <label for="outro">Outro</label>
-                <br><br>
-                <label for="data_nascimento"><b>Data de Nascimento:</b></label>
-                <input type="date" name="data_nascimento" id="data_nascimento" required>
-                <br><br><br>
-                <div class="inputBox">
-                    <input type="text" name="cidade" id="cidade" class="inputUser" required>
-                    <label for="cidade" class="labelInput">Cidade</label>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="text" name="estado" id="estado" class="inputUser" required>
-                    <label for="estado" class="labelInput">Estado</label>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="text" name="endereco" id="endereco" class="inputUser" required>
-                    <label for="endereco" class="labelInput">Endereço</label>
-                </div>
-                <br><br>
-                <input type="submit" name="submit" id="submit">
-            </fieldset>
-        </form>
-    </div>-->
 </body>
 </html>

560202362386a5392fd8acb465aa71ffc4b0d714
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-16 20:18:07 -0300

    Model do usuário criada

---
+++ src/api/src/models/UserModel.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace src\api\src\models;
+
+use PDO;
+use src\config\Connection;
+
+require_once 'vendor/autoload.php';
+// include __DIR__ . '/../database/CarrinhoData.php';
+
+class UserModel {
+  
+  public function retrieveUsers () {
+		
+		// Seleciona o usuário no banco de acordo com o que ele digitou no login, e devolve o resultado para a model.
+		
+		$con = Connection::getConn ();
+		
+		$query = "SELECT * FROM USUARIO";
+		
+		$con = $con -> query ($query);
+			
+		return $con;
+		
+	}
+	
+	public function retrieveUser ($name) {
+		
+		// Seleciona o usuário no banco de acordo com o que ele digitou no login, e devolve o resultado para a model.
+		
+		$con = Connection::getConn ();
+		
+		$query = "SELECT * FROM USUARIO WHERE nome_usuario = '$name'";
+		
+		$con = $con -> query ($query);
+			
+		return $con;
+		
+	}
+  
+}
+
+?>

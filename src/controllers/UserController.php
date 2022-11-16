427f39cf2d0d54e1a605ad9cad465872d884832f
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-16 20:14:49 -0300

    Controller do usuário criada

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,9 +1,31 @@
 <?php
-
 namespace src\controllers;
+
+use src\models\UserModel;
+use src\services\Api;
 
 require_once 'vendor/autoload.php';
 
-class UserController
-{
+class UserController {
+	
+	public function login ($name, $password) {
+		
+		$user = new UserModel ();
+		
+		$user = $user -> login ($name, $password);
+		
+		if ($user [0] -> nome_usuario == $name) {
+		 	
+			$_SESSION ["usuario"] = $user [0]-> nome_usuario;
+			$_SESSION ["cpf"] = $user [0] -> cpf_usuario;
+			$_SESSION ["id"] = $user [0] -> id_usuario;
+			
+			return 1;
+			
+		}
+		
+	}
+	
 }
+
+?>

0f7bdfd6b2b7dfe67e8cdd37f49833049a3b2a74
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-16 20:13:59 -0300

    Model agora conversa com a api

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -2,8 +2,25 @@
 
 namespace src\models;
 
+use src\services\Api;
+use PDO;
+
 require_once 'vendor/autoload.php';
 
-class UserModel
-{
+class UserModel {
+	
+	public function login ($name, $password) {
+		
+		// Aqui é recebido o resultado da query e se a busca retornou resultado, são armazenados os dados do usuário numa sessão que dura
+		// até ele fazer logout. É retornado 1 se tudo ocorreu bem para facilitar o tratamento de erro.
+		
+		$api = new Api();
+		$data = array ("cpf_usuario" => $password);
+		
+		return $api -> user () -> login ($name, $data);
+		
+	}
+		
 }
+
+?>

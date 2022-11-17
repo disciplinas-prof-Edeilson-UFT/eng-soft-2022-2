41f649283447244aed860c618b7aa91ddce6b601
JoaoGabrielh <93159462+JoaoGabrielh@users.noreply.github.com>
2022-11-17 13:55:07 -0300

    Usando a session

--- src/views/pages/Perfil.php
+++ src/views/pages/Perfil.php
@@ -15,11 +15,16 @@ if(!isset($_SESSION['id'])){
 	<body>
 	<div id="info-perfil">
 		<img id="img-info"src = "https://uploaddeimagens.com.br/images/004/064/618/thumb/user.png?1666047148">
+		
 		<div class="info">
-			<h2>Nome do usuario</h2>
-			<p><span>id:</span> 1</p>
-			<p><span>cpf:</span> 000.000.000-00</p>
+			
+			<h2> <?php echo ($_SESSION ["usuario"]); ?> </h2>
+			
+			<p><span>id:</span> <?php echo ($_SESSION ["id"]); ?> </p>
+			<p><span>cpf:</span> <?php echo ($_SESSION ["cpf"]); ?> </p>
+			
 		</div>
+		
 	</div>
 	
 	</body>
